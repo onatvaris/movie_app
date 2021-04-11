48960ded278e563470df20418016953315c8a32f
onatvaris <51972191+onatvaris@users.noreply.github.com>
2021-04-11 08:08:22 +0300

    Update db.js

--- server/helpers/db.js
+++ server/helpers/db.js
@@ -3,8 +3,6 @@ const mongoose = require("mongoose");
 const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_password}@cluster0.umnve.mongodb.net/${process.env.db_host}?retryWrites=true&w=majority`;
 
 module.exports = () => {
-  // const uri =
-  //   "mongodb+srv://onat:benonat123@cluster0.umnve.mongodb.net/movie_app?retryWrites=true&w=majority";
   mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
 
   mongoose.connection.on("open", () => {
